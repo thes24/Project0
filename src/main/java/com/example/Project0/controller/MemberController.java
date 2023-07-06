fc13c302ac71ab7e84bc8632ec39543507ba9759
thes24 <seungwoo624@gmail.com>
2023-07-06 18:12:20 +0900

    post mapping /signup member etc

--- src/main/java/com/example/Project0/controller/MemberController.java
+++ src/main/java/com/example/Project0/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.example.Project0.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/member")
@@ -11,6 +13,13 @@ public class MemberController {
     @RequestMapping("/signup")
     public String suForm() {
         return "signup.html";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@RequestParam String memberEmail, String memberPassword, String memberName) {
+        System.out.println("Member Signup");
+        System.out.println("Email:" + memberEmail + "Password:" + memberPassword + "Name:" + memberName);
+        return "index.html";
     }
 
     //로그인 페이지 출력 요청
