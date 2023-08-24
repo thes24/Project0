3cfe1673eb3af2cddba2954edabf707b37257cc6
thes24 <seungwoo624@gmail.com>
2023-08-25 05:15:49 +0900

    New: add WebMvcConfigurer & login interceptor

---
+++ src/main/java/com/example/Project0/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.Project0.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.Project0.interceptor.LogInInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LogInInterceptor logInInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        
+        // WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(logInInterceptor).addPathPatterns("/member/");
+    }
+}
---
+++ src/main/java/com/example/Project0/interceptor/LogInInterceptor.java
@@ -0,0 +1,69 @@
+package com.example.Project0.interceptor;
+
+import java.lang.reflect.Method;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.Project0.entity.MemberEntity;
+import com.example.Project0.services.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class LogInInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+                
+        HttpSession session = request.getSession();
+        System.out.println("attribute1 " + session.getAttribute("memberId"));
+        if (session.getAttribute("memberId") instanceof Long) {
+            System.out.println("Long");
+        }
+        Long id = (Long) session.getAttribute("memberId");
+        System.out.println("id check" + id);
+        if (memberService == null) {
+            System.out.println("member service null");
+        }
+
+        MemberEntity memberEntity = memberService.getMemberbyId(id);
+
+        if (memberEntity == null) {
+            response.sendError(401, "Bad 401");
+            return false;
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+
+        System.out.println("Bean: " + handlerMethod.getBean());
+        System.out.println("Method: " + method);
+        
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable Exception ex) throws Exception {
+        // TODO Auto-generated method stub
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
