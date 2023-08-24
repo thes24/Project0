685421fa71d0f0db17d521dc40f21ca6a85e12f4
thes24 <seungwoo624@gmail.com>
2023-08-25 07:56:32 +0900

    Docs: add comment

--- src/main/java/com/example/Project0/Project0Application.java
+++ src/main/java/com/example/Project0/Project0Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Project0Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // ./gradlew build && java -jar build/libs/Project0-0.0.1-SNAPSHOT.jar
 		SpringApplication.run(Project0Application.class, args);
 	}
+
 }
