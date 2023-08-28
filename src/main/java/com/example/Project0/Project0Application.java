c136b951bc9793dae1c63bda4e1441eb3b6765ce
thes24 <seungwoo624@gmail.com>
2023-08-29 02:14:50 +0900

    Feat: add CorsRegistry

--- src/main/java/com/example/Project0/Project0Application.java
+++ src/main/java/com/example/Project0/Project0Application.java
@@ -2,12 +2,25 @@ package com.example.Project0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Project0Application {
 
 	public static void main(String[] args) { // ./gradlew build && java -jar build/libs/Project0-0.0.1-SNAPSHOT.jar
 		SpringApplication.run(Project0Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
